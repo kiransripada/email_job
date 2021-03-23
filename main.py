4444fcf6ea471fa7af3673ffa4a74901adcee7a3
kiransripada <kiransripada@users.noreply.github.com>
2021-03-23 11:19:59 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import schedule
+import time
+import smtplib, ssl
+
+port = 465  # For SSL
+password = input("Type your password and press enter: ")
+sender_email = "youremail@gmail.com"
+receiver_email = "youremail+12@gmail.com"
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+context = ssl.create_default_context()
+
+
+def job():
+    print("I'm working...")
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        #server.connect("smtp.gmail.com", 587)
+        #server.helo()
+        server.starttls(context=context)
+        server.login("kiran.sripada.sherwin@gmail.com", password)
+        server.sendmail(sender_email, receiver_email, message)
+
+    except Exception as e:
+        print(e)
+    finally:
+        server.quit()
+
+
+schedule.every(10).seconds.do(job)
+##
+while True:
+    schedule.run_pending()
+    time.sleep(1)

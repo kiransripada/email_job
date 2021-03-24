9d3b8f9d8b44f7e88c8155e26aafd4d4031f6b06
kiransripada <kiransripada@users.noreply.github.com>
2021-03-24 16:57:53 -0400

    trigger by dob

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import schedule
 import time
 import smtplib, ssl
 import csv
-
+import datetime
 
 port = 465  # For SSL
 password = input("Type your password and press enter: ")
@@ -18,7 +18,7 @@ context = ssl.create_default_context()
 
 ##  Job function  to send email .You can write your logi to fetch email ids from csv in a separate function or here it self
 def job():
-    print("Start Email Job..")
+    print("Start Birthday Email Job..")
     try:
 
         server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -26,13 +26,19 @@ def job():
         server.starttls(context=context)
         server.login("youremail@gmail.com", password)
         ## read CSV
-        file= open("email_add.csv")
+        file = open("email_add.csv")
         reader = csv.reader(file)
         next(reader)
         ## Itrate over list
-        for name, email in reader:
-            print(f"Sending email to {name}")
-            server.sendmail(sender_email, email, message)
+        for name, email, dob in reader:
+            print(f"Iterate over email add csv")
+            today = datetime.datetime.now().strftime('%Y-%m-%d')
+            date_diff = days_between(dob, today)
+            print(f"number diff {date_diff}")
+            ##send email if dob is today
+            if date_diff == 0:
+                print(f"Sending email to {name} with  {dob}")
+                server.sendmail(sender_email, email, message)
 
     except Exception as e:
         print(e)
@@ -41,13 +47,20 @@ def job():
 
 
 ## Scheduler & trigger logic  to send email every 10 secs
-#Ref https://pypi.org/project/schedule/
-#schedule.every(10).seconds.do(job)
+# Ref https://pypi.org/project/schedule/
+# schedule.every(10).seconds.do(job)
 ##If you wan to send email every day at 8:00 , uncomment below line
 
+def days_between(d1, d2):
+    d1 = datetime.datetime.strptime(d1, "%Y-%m-%d")
+    d2 = datetime.datetime.strptime(d2, "%Y-%m-%d")
+    return abs((d2 - d1).days)
 
-schedule.every().day.at("8:00").do(job)
+schedule.every(10).seconds.do(job)
+
+#schedule.every().day.at("23:13").do(job)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
+
